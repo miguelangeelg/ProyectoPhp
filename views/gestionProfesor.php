7563e3b32c51a30324b9252d0ae82c1a78022b17
djgfelizzola <64566898+djgfelizzola@users.noreply.github.com>
2020-05-30 01:22:26 -0500

    Cambios #3

--- views/gestionProfesor.php
+++ views/gestionProfesor.php
@@ -1,109 +1,52 @@
 <?php
 include("./componentes/navegacion.php");
 
-?>
-
-
-        
- 
+?>      
         <div id="contentWrapper">
-            <section class="form_wrap">
-
-            <section class="cantact_info">
-    <section class="info_title">
-        <span class="fa fa-user-circle"></span>
-        <h2>INFORMACIÓN DEL PROFESOR</h2>
-    </section>
-    <center>
-    <section align="center" class="info_items">
-        <p><span class="fa fa-envelope"></span>Documento : 100074612</p>
-        <p><span class="fa fa-mobile"></span>Nombres : Fulanito De Tal</p>
-        <p><span class="fa fa-mobile"></span>Correo : correo@correo.com</p>
-        <p><span class="fa fa-mobile"></span>Teléfono : 333022232</p>
-        <p><span class="fa fa-mobile"></span>Profesión : Ingeniero</p>
-
-    </section>
-    </center>
-
-</section>
-
-<form action="" class="form_contact">
-    <div class="tituloCalificaciones">
-    <h2 >Calificaciones</h2>
-    </div>
-
-    <select class="classic">
+        <select class="classic" id="periodo">
   <option>Periodo 1</option>
   <option>Peiodo 2</option>
   <option>Periodo 3</option>
 </select>
 
-<?php include("./componentes/botonGenerarPdfEstu.php");  ?>
-    <div class="user_info">
 
-<table class="table-scroll small-first-col">
-  <thead>
-    <tr>
-    <th>Head 1</th>
-    <th>Head 2</th>
-    </tr>
-  </thead>
-  <tbody class="body-half-screen">
-    <tr>
-      <td>1</td>
-      <td>First row</td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>2</td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>2</td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>2</td>
-    </tr><tr>
-      <td>1</td>
-      <td>2</td>
-    </tr><tr>
-      <td>1</td>
-      <td>2</td>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>2</td>
-    </tr><tr>
-      <td>1</td>
-      <td>2</td>
-    </tr><tr>
-      <td>1</td>
-      <td>2</td>
-    </tr><tr>
-      <td>1</td>
-      <td>2</td>
-    </tr><tr>
-      <td>1</td>
-      <td>2</td>
-    </tr><tr>
-      <td>1</td>
-      <td>2</td>
-    </tr><tr>
-      <td>1</td>
-      <td>2</td>
-    </tr><tr>
-      <td>1</td>
-      <td>2</td>
-    </tr>
-  </tbody>
-</table>
+        <select class="classic" id="materia">
+  <option>Materia 1</option>
+  <option>Materia 2</option>
+  <option>Materia 3</option>
+</select>
+
+        <select class="classic" id="grupo">
+  <option>Grupo 1</option>
+  <option>Grupo 2</option>
+  <option>grupo 3</option>
+</select>
+
+        <form action="" class="form_contact">
+    <h2>ASIGNACIÓN DE CALIFICACIONES</h2>
+    <div class="user_info">
+        <label for="names">Documento: *</label>
+        <input type="number" id="names" required style="border-radius: 20px / 10px;" >
+
+        <label for="phone">Nota 1:</label>
+        <input type="number" id="phone" style="border-radius: 20px / 10px;">
+
+        <label for="phone">Nota 2:</label>
+        <input type="number" id="phone" style="border-radius: 20px / 10px;">
+
+        <label for="phone">Nota 3:</label>
+        <input type="number" id="phone" style="border-radius: 20px / 10px;">
+
+        <label for="phone">Nota 4:</label>
+        <input type="number" id="phone" style="border-radius: 20px / 10px;">
+
+        <label for="phone">Nota 5:</label>
+        <input type="number" id="phone" style="border-radius: 20px / 10px;">
+
+        <input type="submit" class="btn btn-success" value="Consultar" style="background-color:#4091EC; color: white;border:gainsboro 8px outset;border-radius: 20px / 10px;" >
+        <input type="submit" class="btn btn-success" value="Asignar" style="background-color:#4091EC; color: white;border:gainsboro 8px outset;border-radius: 20px / 10px;" >
     </div>
 </form>
-
-</section>
-
-
         </div><!--End of Content Wrapper-->
    <?php 
     include("./componentes/footer.php");

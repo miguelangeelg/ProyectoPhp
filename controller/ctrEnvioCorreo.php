97c016bbb823105dbeb871520c0ee01a4ab57470
Alahingq <54775794+Alahingq@users.noreply.github.com>
2020-06-05 10:57:41 -0500

    CORREO
    S

--- controller/ctrEnvioCorreo.php
+++ controller/ctrEnvioCorreo.php
@@ -1,24 +1,24 @@
 <?php
-//if(isset($_POST["name"],$_POST["phone"],$_POST["email"],$_POST["message"]) and  $_POST["message"]=!"" and $_POST["phone"]!="" and $_POST["email"]!="" and $_POST["name"]!=""){
-$nombres=$_POST["name"];
-$telefono=$_POST["phone"];
-$correo=$_POST["email"];
-$mensaje=$_POST["message"];
-$destinatario="djgfelizzola@gmail.com";
-$asunto="MENSAJE DE $nombres";
-$carta="De: $nombres\n";
-$carta.="Correo: $correo\n";
-$carta.="Telefono: $telefono\n";
-$carta.="Mensaje: $mensaje\n";
-if(mail($destinatario,$asunto,$carta))
-{
-   echo "<script>alert('Funcion \"mail()\" ejecutada, por favor verifique su bandeja de correo.');</script>";
-}else{
-   echo "<script>alert('No se pudo enviar el mail, por favor verifique su configuracion de correo SMTP saliente.');</script>";
+if(isset($_POST["name"],$_POST["phone"],$_POST["email"],$_POST["message"]) and  $_POST["message"]=!"" and $_POST["phone"]!="" and $_POST["email"]!="" and $_POST["name"]!=""){
+   $nombres=$_POST["name"];
+   $telefono=$_POST["phone"];
+   $correo=$_POST["email"];
+   $mensaje=$_POST["message"];
+   $destinatario="djgfelizzola@gmail.com";
+   $asunto="MENSAJE DE $nombres";
+   $carta="De: $nombres\n";
+   $carta.="Correo: $correo\n";
+   $carta.="Telefono: $telefono\n";
+   $carta.="Mensaje: $mensaje\n";
+      if(mail($destinatario,$asunto,$carta))
+      {
+         echo "<script>alert('Funcion \"mail()\" ejecutada, por favor verifique su bandeja de correo.');</script>";
+      }else{
+         echo "<script>alert('No se pudo enviar el mail, por favor verifique su configuracion de correo SMTP saliente.');</script>";
+      }
 }
-
-/*else{
-   // echo "DEBE RELLENAR TODOS LOS CAMPOS PERRA";
-}*/
+else{
+    echo "DEBE RELLENAR TODOS LOS CAMPOS PERRA";
+}
 
 ?>
